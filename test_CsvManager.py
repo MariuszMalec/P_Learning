4cd65d6eb0fd66fa3129665a7adedafc6b460b25
MariuszMalec <mario106c@gmail.com>
2025-06-04 10:59:57 +0200

    fix init_service

--- test_CsvManager.py
+++ test_CsvManager.py
@@ -1,23 +1,25 @@
-import unittest, os, sys
-import P_CsvManager as Solution
+import os
+import sys
+import unittest
+
 from P_CsvService import CsvManager
-import P_Logger
+
+
+def init_service():
+    folder = r'./Source'
+    if not (os.path.dirname(folder)):
+        sys.exit()
+    file = os.path.join(folder, "LADT1100P1002.csv")
+    service = CsvManager()
+    service.setFileName(file)
+    service.load()
+    return service
 
 
 class test_P_CsvService(unittest.TestCase):
-    
-    def setUp(self):
-        self.service = self.init_service()
 
-    def init_service(self):
-        folder = r'./Source'
-        if not (os.path.dirname(folder)):
-                sys.exit()
-        file = os.path.join(folder, "LADT1100P1002.csv")    
-        service = CsvManager()        
-        service.setFileName(file)
-        service.load()
-        return service
+    def setUp(self):
+        self.service = init_service()
 
     def test_hasVar_WhenFound_ReturnTrue(self):
         result = self.service.hasVar("DN")
@@ -25,11 +27,12 @@ class test_P_CsvService(unittest.TestCase):
 
     def test_hasVar_WhenNotFound_ReturnFalse(self):
         result = self.service.hasVar("test")
-        self.assertEqual(result, False)        
+        self.assertEqual(result, False)
 
     def test_getVar_WhenFound_ReturnValue(self):
         result = self.service.getVar("DN")
-        self.assertEqual(result, "559.7")        
+        self.assertEqual(result, "559.7")
+
 
 if __name__ == '__main__':
     unittest.main()
